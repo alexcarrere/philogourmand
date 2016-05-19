588e90c4ee345180426f3404bc1874fc18f86e2e
BumbleBrice <collilieux.brice@gmail.com>
2016-05-19 16:33:10 +0200

    liens
    Ajout très rapide des liens vers les pages utils

--- admin/administration.php
+++ admin/administration.php
@@ -19,6 +19,11 @@ else {
 
 	echo 'vous n\'êtes pas autorisé à aporter des modifications';
 }
-?>
+?> 
+
+<a href="view_recipes.php" class="btn btn-default">Les recettes</a><br>
+<a href="add_recipe.php" class="btn btn-default">Créer une recette</a><br>
+<a href="view_users.php" class="btn btn-default">Les utilisateurs</a><br>
+<a href="add_user.php" class="btn btn-default">Créer un utilisateur</a><br>
 </body>
 </html>
