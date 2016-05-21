715ded50c70212158b029cfed85adbde7d4ebe26
BumbleBrice <collilieux.brice@gmail.com>
2016-05-21 14:48:56 +0200

    Réctification
    Pour évité que ça supprime un utilisateur ....

--- admin/delete_recipe.php
+++ admin/delete_recipe.php
@@ -20,9 +20,9 @@ if (!empty($_SESSION) && isset($_SESSION['user']['role'])){
 
 if (!empty($_GET) && isset($_GET['id'])) {
 	//On vérifie l'id du user
-	$id_user = checkId($_GET['id']);
+	$id_recipe = checkId($_GET['id']);
 	//si l'id est incorrect (NULL)
-	if(empty($id_user)) {
+	if(empty($id_recipe)) {
 		$error[] = 'le recette recherché n\'existe pas';
 	}
 
@@ -30,16 +30,16 @@ if (!empty($_GET) && isset($_GET['id'])) {
 
 		if(isset($_GET['confirm']) && $_GET['confirm'] == 'ok') {
 	 		$del = $pdo->prepare('DELETE FROM recipes WHERE id = :id');
-	 		$del->bindValue(':id',$id_user,PDO::PARAM_INT);
+	 		$del->bindValue(':id',$id_recipe,PDO::PARAM_INT);
 	 		if($del->execute()) {
 	 				$_SESSION['del_recipe'] = 'ok';
-	 				header('Location: view_users.php');
+	 				header('Location: view_recipes.php');
 	 				die;		
 			} 
 	 	}
 	 	else {
 				  $res = $pdo->prepare('SELECT * FROM recipes WHERE id = :id');
-				  $res->bindValue(':id', $id_user, PDO::PARAM_INT);
+				  $res->bindValue(':id', $id_recipe, PDO::PARAM_INT);
 				  $res->execute();
 
 				  $recette = $res->fetch(PDO::FETCH_ASSOC);
@@ -59,11 +59,11 @@ if (count($error) > 0) : ?>
 include_once '../inc/header_admin.php';
 ?>
 <div class="alert alert-danger" role="alert">
-<p> ATTENTION ! Vous souhaitez surprimé la recette <?= $recette['title'] ?> La sentence sera irrémédiable !!!!</p>
+<p> ATTENTION ! Vous souhaitez surprimé la recette <?= $recette['title'] ?>!!! La sentence sera irrévocable !!!!</p>
 
 
 
 </div>
-<a type="button" class="btn btn-danger" href="delete_user.php?id=<?php echo $id_user;?>&confirm=ok">Cliquez ici si vous souhaitez vraiment supprimer l'recette</a>
+<a type="button" class="btn btn-danger" href="delete_recipe.php?id=<?php echo $id_recipe;?>&confirm=ok">Cliquez ici si vous souhaitez vraiment supprimer l'recette</a>
 
 <?php include_once '../inc/footer_admin.php'; ?>
