4ba871f69b51f7eb81682997792afcbf40652835
Hugues0111 <hugues.fonteyraud@wanadoo.fr>
2016-05-22 10:42:08 +0200

    j'envoi aussi une version de modification du mot de pass qui fonctionne je regarde pour l'envoi d'un mail

--- admin/lost_password.php
+++ admin/lost_password.php
@@ -3,7 +3,7 @@
 session_start(); 
 // On se connecte à la base de données
 require_once '../inc/connect.php';
-require_once '../vendor/autoload.php';
+//require_once '../vendor/autoload.php';
 
 $error = [];
 $post = [];
@@ -60,6 +60,7 @@ if(!empty($_POST)){
 	}
 	// Ici on traite le formulaire de mise à jour du mot de passe
 	elseif(isset($post['action']) && $post['action'] == 'updatePassword'){
+		
 		// Le mot de passe doit faire entre 8 et 20 caractères
 		if(strlen($post['new_password']) < 8 || strlen($post['new_password']) > 20){
 			$error[] = 'Le mot de passe doit comporter entre 8 et 20 caractères';
@@ -68,8 +69,10 @@ if(!empty($_POST)){
 		if($post['new_password'] != $post['new_password_conf']){
 			$error[] = 'Les mots de passe doivent correspondre!';
 		}
-
 		if(count($error) == 0){ // Il n'y a pas d'erreurs dans le formulaire, on peut vérifier le token & l'adresse email ... et même la date d'expiration
+
+
+
 			$tok = $pdo->prepare('SELECT * FROM tokens_password WHERE email = :postEmail AND token = :postToken AND date_exp > NOW()');
 			$tok->bindValue(':postEmail', $post['email']);
 			$tok->bindValue(':postToken', $post['token']);
@@ -81,6 +84,8 @@ if(!empty($_POST)){
 				$error[] = 'Le token et l\'adresse email ne correspondent pas.'; // Ou le token est expiré, mais on va pas trop donner d'infos quand même :-)
 			}
 			else {
+
+				
 				// Ici, on peut ENFIN changer ce putain de mot de passe :-)
 				$update = $pdo->prepare('UPDATE users SET password = :newPassword WHERE email = :email');
 				$update->bindValue(':newPassword', password_hash($post['new_password'], PASSWORD_DEFAULT)); // On insère le mot de passe hashé
@@ -92,13 +97,20 @@ if(!empty($_POST)){
 					$delete = $pdo->prepare('DELETE FROM tokens_password WHERE id = :idToken');
 					$delete->bindValue(':idToken', $tokenExist['id'], PDO::PARAM_INT); // $tokenExist contient les infos de mon token extraites de la base de données.. et donc son ID
 					$delete->execute();
+
+				
 					header('Location: login.php');
 					die;
 				}
 
 			}
 
-		}
+
+
+
+
+
+		}//fin count($error) == 0
 	}
 
 }
@@ -137,61 +149,7 @@ if(!empty($_POST)){
 
 
 
-				<?php
-
-					$mail = new PHPMailer;
-
-											//$mail->SMTPDebug = 3;                               // Enable verbose debug output
-
-											$mail->isSMTP();                                      // Set mailer to use SMTP
-											/*$mail->Host = 'smtp1.example.com;smtp2.example.com';*/  // Specify main and backup SMTP servers
-											$mail->Host = '';
-											$mail->SMTPAuth = true;                               // Enable SMTP authentication
-											/*$mail->Username = 'user@example.com'; */                // SMTP username
-											$mail->Username = ''; 
-											/*$mail->Password = 'secret'; */
-											$mail->Password = '';                           // SMTP password
-											$mail->SMTPSecure = '';                            // Enable TLS encryption, `ssl` also accepted
-											$mail->Port = 587;                                    // TCP port to connect to
-
-											$mail->setFrom('monsite', 'amoi');
-											$mail->addAddress('@', '');     // Add a recipient
-											/*$mail->addAddress('ellen@example.com');*/               // Name is optional deuxieme adresse 
-											//$mail->addReplyTo('info@example.com', 'Information');
-											/*$mail->addCC('cc@example.com');
-											$mail->addBCC('bcc@example.com');*/
-
-											/*$mail->addAttachment('/var/tmp/file.tar.gz');  */       // Add attachments
-											/*$mail->addAttachment('/tmp/image.jpg', 'new.jpg'); */   // Optional name
-											$mail->isHTML(true);                                  // Set email format to HTML
-
-											$mail->Subject = 'Here is the subject';
-											$mail->Body    = '<p>Vous pouvez réinitialiser votre mot de passe en cliquant sur le lien suivant :<br>
-											<a href="'.$linkChangePassword.'">Modifier mon mot de passe</a>	</p><br>				
-											<code>'.$linkChangePassword.'</code>';
-
-											$mail->AltBody = 'Vous pouvez réinitialiser votre mot de passe en cliquant sur le lien suivant :'.$linkChangePassword;
-
-										
-
-
-											if(!$mail->send()) {
-											    echo 'Message could not be sent.';
-											    echo 'Mailer Error: ' . $mail->ErrorInfo;
-											} else {
-											    echo 'Message has been sent';
-											}
-
-
-
-
-
-
-
-
-
-
-				?>
+				
 				<p>Vous pouvez réinitialiser votre mot de passe en cliquant sur le lien suivant :
 				<br>
 				<a href="<?=$linkChangePassword; ?>">Modifier mon mot de passe</a>
