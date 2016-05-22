a7518377acf741a76085b6f7098af223a09bad9a
BumbleBrice <collilieux.brice@gmail.com>
2016-05-22 20:10:58 +0200

    ajout d'une id

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ $recettes = $res->fetchAll(PDO::FETCH_ASSOC);
 
 foreach($recettes as $recipe){ 
 ?>
-        <div class="col-md-4">
+        <div id="img_recette_index" class="col-md-4">
             <img class="img-responsive" alt="entree" src="img/<?php echo $recipe['link']; ?>">
             <br>    
             <p style="text-align:center;"><a  href="view_recipe.php" class="link_recipes">Lire la recette</a></p>
